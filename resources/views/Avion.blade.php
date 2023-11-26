fe44cad9ff9a46720acf3359ebcab060b5271bc5
Erick Daniel Escoto Perez <erickdanielescoto@gmail.com>
2023-11-25 21:08:26 -0600

    avioncito

--- resources/views/Avion.blade.php
+++ resources/views/Avion.blade.php
@@ -12,70 +12,26 @@
 <body>
     <div id="avion">
         <div id="premium">
-            <div id="A1" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-            <div id="B1" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-            <span class="material-symbols-outlined"></span>
-            <div id="E1" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-            <div id="F1" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-            <div id="A2" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-            <div id="B2" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>            
-            <span class="material-symbols-outlined"></span>
-            <div id="E2" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-            <div id="F2" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-            <div id="A3" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-            <div id="B3" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-            <span class="material-symbols-outlined"></span>
-            <div id="E3" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-            <div id="F3" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
         </div>
         <div id="ejecutiva">
-        <div id="A5" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-        <div id="B5" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-        <div id="C5" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-        <span class="material-symbols-outlined"></span>
-        <div id="D5" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-        <div id="E5" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-        <div id="F5" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-        <div id="A6" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-        <div id="B6" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-        <div id="C6" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-        <span class="material-symbols-outlined"></span>
-        <div id="D6" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-        <div id="E6" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-        <div id="F6" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-        <div id="A7" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-        <div id="B7" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-        <div id="C7" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-        <span class="material-symbols-outlined"></span>
-        <div id="D7" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-        <div id="E7" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-        <div id="F7" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-        <div id="A8" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-        <div id="B8" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-        <div id="C8" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-        <span class="material-symbols-outlined"></span>
-        <div id="D8" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-        <div id="E8" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-        <div id="F8" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-        <div id="A9" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-        <div id="B9" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-        <div id="C9" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-        <span class="material-symbols-outlined"></span>
-        <div id="D9" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-        <div id="E9" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-        <div id="F9" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-        <div id="A10" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-        <div id="B10" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-        <div id="C10" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-        <span class="material-symbols-outlined"></span>
-        <div id="D10" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-        <div id="E10" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
-        <div id="F10" class="asiento"><span class="material-symbols-outlined">event_seat</span></div>
         </div>
         <div id="pobres">
         </div>
     </div>
-    <button onclick="generarAsientos(120)">Generar</button>
+    <button onclick="generarAsientos(121)">Generar</button>
+    <div id="avionDiseño">
+        <div id="avionIzquierda">
+            <div id="aletaIzquierda"></div>
+        </div>
+        <div id="avionCentro">
+            <div id="diseñoCabeza"></div>
+            <div id="diseñoCuerpo"></div>
+            <div id="diseñoCola"></div>
+        </div>
+        <div id="avionDerecha">
+            <div id="aletaDerecha"></div>
+        </div>
+    </div>
 
 <!-- <span class="material-symbols-outlined">event_seat</span>
 <span class="material-symbols-outlined">chair</span>
