8a5ecf08750fa7e228c5a6a5ed114bb3184dc61e
ale2002solano <ale133valle@gmail.com>
2023-11-26 12:37:37 -0600

    borre carousel

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -47,27 +47,6 @@
   </div>
 </nav>
 
-<div id="carouselExample" class="carousel slide">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="/public/img/ala-avion.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="/public/img/ala-avion.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="/public/img/ala-avion.jpg" class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
 
 <br><br>
 <h1>Vuela más,paga menos: Vuelos baratos en SkyVibe Airline</h1>
