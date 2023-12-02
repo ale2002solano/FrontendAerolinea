ac975027ac388c898ad4b6ea5a1141f7b98f1889
ale2002solano <ale133valle@gmail.com>
2023-12-02 15:22:06 -0600

    x

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
-
+use App\Http\Controllers\VuelosController;
 
 /*
 |--------------------------------------------------------------------------
