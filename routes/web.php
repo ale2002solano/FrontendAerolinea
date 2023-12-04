f93e879049b4f4c1f23e351d3002dedfe7c7027e
ale2002solano <ale133valle@gmail.com>
2023-12-03 22:51:54 -0600

    borrar codigo comentado

--- routes/web.php
+++ routes/web.php
@@ -41,11 +41,9 @@ Route::get('/login', function () {
 
 //BOTONES DEL LOGIN
 Route::get('/home/login/registrarse',[LoginController::class, 'registrarse'])->name('registrarse');
-//FIN BOTONES DEL LOGIN
 
 //BOTONES DEL HOME
 Route::get('/home/login',[LoginController::class, 'index'])->name('login');
-//FIN BOTONES HOME
 
 // CONSUMO DE LAS APIS DE GUZZLE
 Route::post('/login',[LoginController::class, 'login'])->name('login.post');
