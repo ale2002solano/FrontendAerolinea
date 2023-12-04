07ddae32b1009a84b1d9782641543e940f7b8b25
ale2002solano <ale133valle@gmail.com>
2023-12-03 21:59:54 -0600

    Resolver lo que se borro en el merch

--- resources/views/Registro.blade.php
+++ resources/views/Registro.blade.php
@@ -44,44 +44,7 @@
 
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script>
-    $(document).ready(function() {
-        $('#formularioRegistro').submit(function(event) {
-            event.preventDefault(); // Evitar que el formulario se envíe normalmente
-
-            // Obtener la URL base desde la etiqueta meta
-            const baseUrl = $('meta[name="base-url"]').attr('content');
-
-            // Ruta relativa para el registro de clientes
-            const registroRoute = '/registrarCliente';
-
-            // Construir la URL completa dinámica
-            const apiUrl = baseUrl + registroRoute;
-
-            // Obtener los datos del formulario
-            const formData = new FormData(this);
-
-            // Realizar la solicitud AJAX POST a la URL dinámica para el registro de clientes
-            $.ajax({
-                url: apiUrl,
-                method: 'POST',
-                data: formData,
-                processData: false,
-                contentType: false,
-                success: function(data) {
-                    // Manejar la respuesta exitosa aquí
-                    console.log(data);
-                    alert('Cliente registrado exitosamente');
-                },
-                error: function(jqXHR, textStatus, errorThrown) {
-                    // Manejar errores
-                    console.error(errorThrown);
-                    alert('Hubo un error al registrar el cliente');
-                }
-            });
-        });
-    });
-</script>
+    <script src="{{ asset('js/registro.js') }}"></script>
 
     
 </body>
